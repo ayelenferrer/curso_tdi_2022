88593837063961b59372e79bd508cb56af70765b
unknown <you@example.com>
2022-05-29 00:07:04 -0300

    m

---
+++ proyecto_final_2022 - copia/header.php
@@ -0,0 +1,17 @@
+<header>
+            <div class="logo">
+                <a href="#" class="marca">RACHEL BEAUTY</a>
+            </div>
+            <nav>
+                <ul class="nav">
+                    <li class="links"><a href="inicio.html">Inicio</a></li>
+                    <li class="links"><a href="productos.html">Productos</a></li>
+                    <li class="links"><a href="contacto.html">Contacto</a></li>
+                </ul>
+            </nav>
+            
+                <!--FIN - Barra de navegación>
+
+                 <!-Section-->
+                 
+</header>
